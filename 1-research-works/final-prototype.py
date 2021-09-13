4d6ee38c2dfe24ea9a2721fe0f68373168aa6cf9
Ranoya-57 <vranoya1998@gmail.com>
2021-09-13 14:45:26 +0530

    Updated if condition

--- 1-research-works/final-prototype.py
+++ 1-research-works/final-prototype.py
@@ -17,38 +17,31 @@ box_height = y2 - y1
 
 
 # Text to be printed
-cert_text = "Python is an interpreted high-level general-purpose programming language. Its design philosophy. Python is an interpreted high-level general-purpose programming language. Its design"
+cert_text = "Python is an interpreted high-level general-purpose programming language."
 
 
 # Calculating average character width and the number of characters that can be fitter inside the box in one line
 text_width, text_height = draw.textsize(cert_text, font=print_font)
 
-
-per_char_avg_with = text_width // len(cert_text)
-size_of_containment = (box_width - (box_width / 5)) // per_char_avg_with
-
+per_char_avg_width = text_width // len(cert_text)
+print(per_char_avg_width)
+size_of_containment = (box_width - (box_width / 15)) // per_char_avg_width
+print("size of containment", size_of_containment)
+#line_width, line_height = draw.textsize(wrapped_text[0])
 wrapped_text = textwrap.fill(cert_text, width=size_of_containment)
 number_lines = len(wrapped_text.split('\n'))
 
 current_line_hight = number_lines * text_height
 
+print("Text Height :", text_height)
+print("Box Height :", box_height)
+print("current_line Height :", current_line_hight)
+print("No of lines ", number_lines)
 
-print(box_height)
-print(current_line_hight)
-
-if (current_line_hight - text_height > box_height):
-    print("Over the box") 
-
-
-
-
-#line_width, line_height = draw.textsize(wrapped_text[0])
-
-
-#tw, th = draw.textsize(wrapped_text, font = print_font)
-#(tw /2) (th /2)
-
-draw.text((x1 + (box_width / 2), y1 + (box_height / 2)), wrapped_text, font=print_font, fill=(255, 0, 0, 255), anchor = 'mm')
-
-
-im.save('test.png')
+if (current_line_hight < box_height):
+    #tw, th = draw.textsize(wrapped_text, font = print_font)
+    #(tw /2) (th /2)
+    draw.text((x1 + (box_width / 2), y1 + (box_height / 2)), wrapped_text, font=print_font, fill=(255, 0, 0, 255), anchor = 'mm' , align= "center")
+    im.save('test.png')
+else:
+    print("Over the box")
