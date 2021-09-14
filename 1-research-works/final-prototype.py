886f52b3f731dd14e5e50c35829e3ba2170384bc
GunaRakulan-GR <gunarakulan@gmail.com>
2021-09-14 20:37:11 +0530

    Implement auto resizer

--- 1-research-works/final-prototype.py
+++ 1-research-works/final-prototype.py
@@ -3,9 +3,7 @@ from PIL import ImageFont, ImageDraw, Image
 import os
 
 
-print_font = ImageFont.truetype(font='./times.ttf', size=50)
-im = template = Image.open(os.path.join(os.getcwd(), "template.png"))  # Read
-draw = ImageDraw.Draw(template)  # drawing object
+font_size = 30
 
 # Coordinates of the corners of the box in the template
 y1, y2 = 187, 440
@@ -15,33 +13,41 @@ x1, x2 = 63, 1045
 box_width =  x2 - x1
 box_height = y2 - y1
 
-
 # Text to be printed
-cert_text = "Python is an interpreted high-level general-purpose programming language."
+cert_text = "Python is an interpreted high-level general-purpose programming language.Python is an interpreted."
+
+template = Image.open(os.path.join(os.getcwd(), "template.png"))
+drawing_object = ImageDraw.Draw(template)  # drawing object
 
 
-# Calculating average character width and the number of characters that can be fitter inside the box in one line
-text_width, text_height = draw.textsize(cert_text, font=print_font)
+while True:
 
-per_char_avg_width = text_width // len(cert_text)
-print(per_char_avg_width)
-size_of_containment = (box_width - (box_width / 15)) // per_char_avg_width
-print("size of containment", size_of_containment)
-#line_width, line_height = draw.textsize(wrapped_text[0])
-wrapped_text = textwrap.fill(cert_text, width=size_of_containment)
-number_lines = len(wrapped_text.split('\n'))
+    given_font = ImageFont.truetype(font='./times.ttf', size=font_size)
 
-current_line_hight = number_lines * text_height
+    text_width, text_height = drawing_object.textsize(cert_text, font=given_font)
 
-print("Text Height :", text_height)
-print("Box Height :", box_height)
-print("current_line Height :", current_line_hight)
-print("No of lines ", number_lines)
+    per_char_avg_width = text_width // len(cert_text) #avg char with
+    size_of_containment = (box_width - (box_width / 15)) // per_char_avg_width
 
-if (current_line_hight < box_height):
-    #tw, th = draw.textsize(wrapped_text, font = print_font)
-    #(tw /2) (th /2)
-    draw.text((x1 + (box_width / 2), y1 + (box_height / 2)), wrapped_text, font=print_font, fill=(255, 0, 0, 255), anchor = 'mm' , align= "center")
-    im.save('test.png')
-else:
-    print("Over the box")
+    wrapped_text = textwrap.fill(cert_text, width=size_of_containment)
+
+    total_line_hight_of_text = len(wrapped_text.split('\n')) * text_height
+
+
+    if total_line_hight_of_text < box_height:
+
+        #tw, th = draw.textsize(wrapped_text, font = print_font)
+        #(tw /2) (th /2)
+        drawing_object.text((x1 + (box_width / 2), y1 + (box_height / 2)), wrapped_text, font=given_font, fill=(255, 0, 0, 255), anchor = 'mm' , align= "center")
+        template.save('test.png')
+        
+        print(f'Font size: {font_size}')
+        
+        break
+
+
+    font_size = font_size - 1
+
+
+    
+
